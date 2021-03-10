a8f31f94ddbc0dc2e31c3c5a02e727154c954a9d
Shreya H M <54499359+ShreyaManjunath@users.noreply.github.com>
2021-03-10 20:35:54 +0530

    added rows

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Row from './Row'
 
 function App() {
   return (
     <div className="App">
       <h1>welcome</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending now" />
+      
     </div>
   );
 }
---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Row() {
+    return (
+        <div>
+        {/* title */}
+
+
+        {/* container */}
+
+
+    
+            
+        </div>
+    )
+}
+
+export default Row
